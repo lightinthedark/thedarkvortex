002f3fff3d20451f30d5c69ec375f9964eba29d3
David <david@pun.net>
2015-02-12 20:12:13 +0000

    # stupid syntax errors
    Signed-off-by: David <david@pun.net>

--- server/configuration.php
+++ server/configuration.php
@@ -3,9 +3,9 @@ define( 'DS', DIRECTORY_SEPARATOR );
 
 class Config
 {
-	var db_server = 'mysql1103.servage.net';
-	var db_database = 'tdv_server0';
-	var db_pwd = 'QdF7HAUiW6apFmf4eaN7'';
+	var $db_server = 'mysql1103.servage.net';
+	var $db_database = 'tdv_server0';
+	var $db_pwd = 'QdF7HAUiW6apFmf4eaN7';
 }
 
 ?>
