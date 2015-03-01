1f2233f90470d638fd166d299539cc33944674e6
David <david@pun.net>
2015-03-01 10:17:44 +0000

    ^ Units now load according to unit-related conditionals (eg unit_id, or unit_size) + General query builder added to abstract object to facilitate. Should mean all objects load the same way
    Signed-off-by: David <david@pun.net>

--- server/v0.2/game/objects.php
+++ server/v0.2/game/objects.php
@@ -4,8 +4,15 @@
  */
 abstract class ObjectAbstract
 {
+	/*
+	 * abstract properties which child classes must define
+	 * 
+	 * $_object
+	 * $_table
+	 * $_properties
+	 * $_d
+	 */
 	var $_knownObjects = array( 'unit', 'wpnt', 'mapc', 'plyr' );
-	var $_knownCols = array();
 	
 	public function __call( $strName, $arrArgs )
 	{
@@ -23,6 +30,115 @@ abstract class ObjectAbstract
 		return array( 'status'=>'error', 'message'=>'function did not exist' );
 	}
 	
+	/**
+	 * Implements the general behaviour that each column has a matching entry in the internal data array
+	 * Returns by reference so it can be used when binding queries etc
+	 * Should be overridden if the data structure is more complex
+	 * @param string $col  The column name whose data var is required
+	 */
+	protected function &_getVarForCol( $col )
+	{
+		return $this->_d[ $col ];
+	}
+	
+	
+	
+	/**
+	 * Prepares a statement to retrieve this object's data into variables
+	 * convert id=... or coords=... into an array of user-accessible ids
+	 * @return PDOStatement
+	 */
+	protected function _getBoundStmt( $selects, $requirements = null )
+	{
+		$selects = array_intersect( $selects, array_keys( $this->_properties ) );
+		
+		if( is_null( $requirements ) ) {
+			$requirements = $this->_getRequirementsFromRequest();
+		}
+		
+		// convert requirements to where clauses
+		$also = array();
+		$wheres = array();
+		foreach( $requirements as $k=>$v ) {
+			if( substr( $k, 4, 1 ) !== '_' ) {
+				continue; // skip things that obviously aren't [a-z]{4}_.*
+			}
+			
+			$objectType = substr( $k, 0, 4 );
+			$col = substr( $k, 5 );
+			if( $objectType === $this->_object ) {
+				if( isset( $this->_properties[ $col ] ) ) {
+					$wheres[ $col ][] = $this->_toArray( $v );
+				}
+			}
+			else {
+				if( array_search( $objectType, $this->_knownObjects ) !== false ) {
+					$also[ $objectType ] = true;
+				}
+			}
+		}
+		
+		// do "also" things to get arrays of ids that this table can use
+		// ****
+		
+		// reduce all where-in-value-list clauses to a single array each
+		foreach( $wheres as $col=>$vals ) {
+			$vals = $this->_flattenArrays( $vals );
+			$wheres[ $col ] = $vals;
+			$whereStrs[ $col ] = $this->_createInClause( $col, $vals );
+		}
+		
+		// put all "where" clauses into a string for the prepared statement
+		$where = empty( $whereStrs )
+			? ''
+			: "\n".'WHERE '.implode( "\n AND ", $whereStrs );
+		
+		// create the statement
+		$db = Database::getDB();
+		
+		$st = $db->prepare(
+				'SELECT '.implode( ', ', $selects )
+				."\n".'FROM '.$this->_table
+				.$where
+				."\n".'ORDER BY id ASC' );
+		
+		// bind all "where" clauses
+		foreach( $wheres as $col=>$vals ) {
+			$this->_bindInClause( $col, $vals, $st, PDO::PARAM_INT );
+		}
+		
+		// execute prep statement to get all requested data that match criteria
+		$st->execute();
+//		var_dump( 'query', $st->queryString );
+//		var_dump( 'err:', $st->errorInfo() );
+		
+		foreach( $selects as $col ) {
+			$st->bindColumn( $col, $this->_getVarForCol( $col ), $this->_properties[ $col ][ 'pdo_type' ] );
+		}
+		
+		return $st;
+	}
+	
+	/**
+	 * Extract json-decoded requirements from a GET request
+	 * 
+	 * @return array  The requirements specified in the GET
+	 */
+	protected function _getRequirementsFromRequest()
+	{
+		$r = array();
+		foreach( $_GET as $k=>$v ) {
+			$r[ $k ] = json_decode( $v );
+		}
+		return $r;
+	}
+	
+	/**
+	 * Ensure the given value is an array, making it so if it isn't
+	 * 
+	 * @param multitype:mixed $v  The value to array-ify
+	 * @return array  The array $v or array( $v )
+	 */
 	protected function _toArray( $v )
 	{
 		if( !is_array( $v ) ) {
@@ -40,15 +156,15 @@ abstract class ObjectAbstract
 	 * Combine a bunch of arrays into a single array
 	 * 
 	 * "reduce?"
-	 * @param unknown $ids
+	 * @param array $ids  An array of arrays of vaules to merge
 	 * @return multitype:
 	 */
-	protected function _flattenIds( $ids )
+	protected function _flattenArrays( $ids )
 	{
 		$ret = array();
 		foreach( $ids as $arr ) {
 			foreach( $arr as $id ) {
-				$ret[ (int)$id ] = true;
+				$ret[ $id ] = true;
 			}
 		}
 		
@@ -57,7 +173,7 @@ abstract class ObjectAbstract
 	
 	protected function _createInClause( $col, $vals )
 	{
-		$colPos = array_search( $col, $this->_knownCols );
+		$colPos = array_search( $col, array_keys( $this->_properties ) );
 		$str = $col.' IN ( NULL'; // invalid id will never add to results, gives starter for comma-separated list
 		if( !( empty( $vals ) || $colPos === false ) ) {
 			foreach( $vals as $k=>$v ) {
@@ -71,7 +187,7 @@ abstract class ObjectAbstract
 	
 	protected function _bindInClause( $col, $vals, $st, $dataType )
 	{
-		$colPos = array_search( $col, $this->_knownCols );
+		$colPos = array_search( $col, array_keys( $this->_properties ) );
 		if( !( empty( $vals ) || $colPos === false ) ) {
 			foreach( $vals as $k=>$v ) {
 				$st->bindValue( ':'.$colPos.'_'.$k, $v, $dataType );
--- server/v0.2/game/objects/units.php
+++ server/v0.2/game/objects/units.php
@@ -1,12 +1,21 @@
 <?php
-require( 'game'.DS.'objects.php' );
+require_once( 'game'.DS.'objects.php' );
 
 /**
  * Units are the players' agents in the game world
  */
 class ObjectUnits extends ObjectAbstract
 {
-	var $_knownCols = array( 'id', 'player_id', 'size', 'color' );
+	var $_object = 'unit';
+	var $_table  = 'units';
+	var $_properties = array(
+		'id'       =>array( 'pdo_type'=>PDO::PARAM_INT ),
+		'player_id'=>array( 'pdo_type'=>PDO::PARAM_INT ),
+		'size'     =>array( 'pdo_type'=>PDO::PARAM_INT ),
+		'color'    =>array( 'pdo_type'=>PDO::PARAM_STR )
+	);
+	var $_d = array();
+	
 	
 	/**
 	 * Alias for getCore
@@ -20,15 +29,15 @@ class ObjectUnits extends ObjectAbstract
 	 */
 	public function getCore()
 	{
-		$st = $this->_getBoundStmt();
+		$st = $this->_getBoundStmt( array( 'id', 'player_id', 'size', 'color' ) );
 		
 		$r = array();
 		while( $row = $st->fetch( PDO::FETCH_BOUND ) ) {
-			$r[ $this->_d['id'] ] = array(
-				'id'=>$this->_d['id'],
-				'player_id'=>$this->_d['player_id'],
-				'size'=>$this->_d['size'],
-				'color'=>$this->_d['color']
+			$r[ $this->_d[ 'id' ] ] = array(
+				'id'=>$this->_d[ 'id' ],
+				'player_id'=>$this->_d[ 'player_id' ],
+				'size'=>$this->_d[ 'size' ],
+				'color'=>$this->_d[ 'color' ]
 			);
 		}
 		
@@ -37,116 +46,31 @@ class ObjectUnits extends ObjectAbstract
 	
 	public function getIds()
 	{
-		return $this->_calcIds();
-	}
-	
-	/**
-	 * Prepares a statement to retrieve waypoint data into variables
-	 * @return PDOStatement
-	 */
-	private function _getBoundStmt()
-	{
-		$db = Database::getDB();
+		$st = $this->_getBoundStmt( array( 'id' ) );
 		
-		$ids = $this->_calcIds();
-		$where = $this->_createInClause( 'id', $ids );
-		
-		$st = $db->prepare(
-			      'SELECT id, player_id, size, color'
-			."\n".'FROM units'
-			."\n".'WHERE '.$where
-			."\n".'ORDER BY id ASC' );
-		$this->_bindInClause( 'id', $ids, $st, PDO::PARAM_INT );
-		$st->execute();
-// 		var_dump( $st->queryString );
-// 		var_dump( 'err:', $st->errorInfo() );
-		
-		$st->bindColumn( 'id',        $this->_d['id'],        PDO::PARAM_INT );
-		$st->bindColumn( 'player_id', $this->_d['player_id'], PDO::PARAM_INT );
-		$st->bindColumn( 'size',      $this->_d['size'],      PDO::PARAM_INT );
-		$st->bindColumn( 'color',     $this->_d['color'],     PDO::PARAM_STR );
-		
-		return $st;
-	}
-	
-	/**
-	 * convert id=... or coords=... into an array of user-accessible ids
-	 */
-	private function _calcIds( $requirements = null )
-	{
-		return array( 2, 4 );
-		if( is_null( $requirements ) ) {
-			$requirements = $_GET;
+		$r = array();
+		while( $row = $st->fetch( PDO::FETCH_BOUND ) ) {
+			$r[ $this->_d[ 'id' ] ] = array(
+				'id'=>$this->_d[ 'id' ],
+			);
 		}
 		
-		$also = array();
-		$wheres = array();
-		$ids = array();
-		foreach( $requirements as $k=>$v ) {
-			if( substr( $k, 4, 1 ) !== '_' ) {
-				continue; // skip things that obviously aren't [a-z]{4}_.*
-			}
-			
-			$objectType = substr( $k, 0, 4 );
-			if( $objectType === 'unit' ) {
-				switch( substr( $k, 5 ) ) {
-					case( 'id' ):
-						$ids[] = $this->_toArray( $v );
-					break;
-					
-					case( 'player_id' ):
-						$wheres[ 'player_id' ] = $v;
-					break;
-				}
-			}
-			else {
-				if( array_search( $objectType, $this->_knownObjects ) !== false ) {
-					$also[ $objectType ] = true;
-				}
-			}
-		}
-		var_dump( 'ids,wheres,also', $ids, $wheres, $also );
-		
-		// do "also" things to get arrays of unit ids
-		
-		// reduce unit ids from "also"s and from basic 'id' case to single array
-		// andadd them to the where clause (restrict to only found ids)
-		$ids = $this->_flattenIds( $ids );
-		$wheres[ 'id' ] = $ids;
-		
-		// put all "where" clauses into a prep statement
-		foreach( $wheres as $col=>$vals ) {
-			$this->_createInClause( $col, $vals );
-		}
-		
-		// create the statement
-		$db = Database::getDB();
-		
-		$st = $db->prepare(
-				'SELECT id'
-				."\n".'FROM units'
-				."\n".'WHERE '.$where
-				."\n".'ORDER BY id ASC' );
-		
-		// bind all "where" clauses
-		foreach( $wheres as $col=>$vals ) {
-			$this->_bindInClause( $col, $vals, $st, PDO::PARAM_INT );
-		}
-		
-		// execute prep statement to get all unit ids that match criteria
-		$st->execute();
-		var_dump( 'err:', $st->errorInfo() );
-		
-		$st->bindColumn( 'id',        $this->_d['id'],        PDO::PARAM_INT );
-		
-		// get all the data
-		
-		return array( 2, 4 );
+		return $r;
 	}
 	
 	protected function getPlayerIds( $requirements )
 	{
+		$st = $this->_getBoundStmt( array( 'id', 'player_id' ) );
 		
+		$r = array();
+		while( $row = $st->fetch( PDO::FETCH_BOUND ) ) {
+			$r[ $this->_d[ 'id' ] ] = array(
+				'player_id'=>$this->_d[ 'player_id' ],
+			);
+		}
+		
+		return $r;
 	}
+
 }
 ?>
